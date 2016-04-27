b62146167694701ae3f12ec678d8389ded7f744a
piotrkardys <piotrk@mobilewebpro.pl>
2016-04-27 17:32:03 +0200

    Lesson2: diference between 'var' and 'let'

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ﻿"LEARN ES6 (ECMASCRIPT 2015)" 
 "27.04.2016 - 16:05 - start of the project" 
 "27.04.2016 - 16:55 – Lesson1: configuration of the project &changing casual functions into ES6 arrow functions; create files: index.html, main.js, webpack.config.js" 
+"27.04.2016 – 17:30 – Lesson2: diference between 'var' and 'let', update file main.js" 
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* LESSON 1 */
+/* LESSON 1 */ /*
 var createGreeting = function (message, name) {				//casual function
 	return message + name;
 };
@@ -36,4 +36,65 @@ var deliveryBoy1 = {
 	}
 }
 deliveryBoy1.receive();
+//------------------------------------------------------
+*/
+
+/* LESSON 2 */
+var message = "hi";
+{
+	var message = "bye";
+}
+console.log(message);			//result of console.log -> "bye" (variable was reassigned)
+
+let message1 = "hi";
+{
+	let message1 = "bye";
+}
+console.log(message1);			//result of console.log -> "hi" (let is block-sensitive and doesn't change the global variable)
+//------------------------------------------------------
+var fs = [];					//array of the functions
+
+for (var i = 0; i < 10; i++) {
+	fs.push(function() {
+		console.log(i);
+	})
+}
+fs.forEach(function (f) {		//result -> 10 times prints number 10 (because var i is already on that number - because of the for loop which pushing functions inside the array)
+	f();
+})
+
+var fs1 = [];
+
+for (let i = 0; i < 10; i++) {	//result -> prints numbers from 0 to 9 (i isn't overwritten and starts from the top - number 0)
+	fs1.push(function() {
+		console.log(i);
+	})
+}
+fs1.forEach(function (f) {
+	f();
+})
+//------------------------------------------------------
+function varFunc() {			//fields will be overwritten
+	var previous = 0;
+	var current = 1;
+	var i;
+	var tmp;
+
+	for (i = 0; i < 10; i += 1) {
+		tmp = previous;
+		previous = current;
+		current = tmp + current;
+	}
+}
+
+function letFunc() {
+	let previous = 0;
+	let current = 1;
+
+	for (let i = 0l i < 10; i += 1) {
+		let tmp = previous;
+		previous = current;
+		current = tmp + current;
+	}
+}
 //------------------------------------------------------
